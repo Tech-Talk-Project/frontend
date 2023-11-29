aa1b8cc22a5f383fea22bd7cd21fd6cc414d8f67
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2023-11-29 19:50:50 +0900

    Feat: Errorboundary에 reset 함수 추가

--- src/components/Error/ErrorBoundary.jsx
+++ src/components/Error/ErrorBoundary.jsx
@@ -4,11 +4,22 @@ class ErrorBoundary extends React.Component {
   constructor(props) {
     super(props);
     this.state = { hasError: false, info: null };
+    this.resetErrorBoundary = this.resetErrorBoundary.bind(this);
   }
 
   static getDerivedStateFromError(error) {
     // 다음 렌더링에서 폴백 UI가 보이도록 상태를 업데이트 합니다.
     return { hasError: true, info: error };
+  }
+
+  resetErrorBoundary() {
+    this.props.onReset();
+
+    // 에러 상태를 기본으로 초기화합니다.
+    this.setState({
+      hasError: false,
+      error: null,
+    });
   }
 
   // componentDidCatch(error, errorInfo) {
@@ -22,7 +33,9 @@ class ErrorBoundary extends React.Component {
 
     if (hasError) {
       // 폴백 UI를 커스텀하여 렌더링할 수 있습니다.
-      return <this.props.fallback error={info} />;
+      return (
+        <this.props.fallback error={info} reset={this.resetErrorBoundary} />
+      );
     }
     return children;
   }
