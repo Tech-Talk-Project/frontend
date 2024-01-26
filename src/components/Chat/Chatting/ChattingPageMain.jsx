2ef3e7058daeb5f60a35c24828bbfda82967cd7f
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2024-01-26 14:02:39 +0900

    Style: 주석 삭제 및 미사용 라이브러리 삭제

--- src/components/Chat/Chatting/ChattingPageMain.jsx
+++ src/components/Chat/Chatting/ChattingPageMain.jsx
@@ -1,105 +1,51 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
-import { Client } from "@stomp/stompjs";
-// import { WebSocket } from "ws";
-import { jwtDecode } from "jwt-decode";
 import { CHAT_QUERY_KEYS } from "../../../constants/queryKeys";
 import { getChattingData } from "../../../apis/chat";
-import { getCookie } from "../../../utils/cookie";
 import useChat from "./useChat";
-import { Input } from "@material-tailwind/react";
-
-// Object.assign(global, { WebSocket });
+import { jwtDecode } from "jwt-decode";
+import { getCookie } from "../../../utils/cookie";
 
 export default function ChattingPageMain({ chatRoomId }) {
-  // const chatClient = useRef();
+  const [chat, setChat] = useState("");
+  const [chatList, setChatList] = useState([]);
   const { error, data } = useQuery({
     queryKey: CHAT_QUERY_KEYS.chatData(chatRoomId),
     queryFn: () => getChattingData({ chatRoomId }),
   });
-  const [chat, setChat] = useState("");
-  const [chattingList, setChattingList] = useState([]);
   const { connect, disconnect, sendMessage } = useChat(
     chatRoomId,
-    setChattingList,
+    jwtDecode(getCookie("accessToken")).memberId,
+    setChatList,
     setChat
   );
 
   const handleChange = (e) => {
-    // console.log(e.target.value);
     setChat(e.target.value);
   };
   const handleClick = () => {
     sendMessage(chat);
   };
-  // const connect = () => {
-  //   chatClient.current = new Client({
-  //     brokerURL: "ws://localhost:8080/ws",
-  //     connectHeaders: {
-  //       memberId: jwtDecode(getCookie("accessToken")).memberId,
-  //       chatRoomId,
-  //     },
-  //     debug: function (str) {
-  //       console.log(str);
-  //     },
-  //     onConnect: () => {
-  //       subscribe();
-  //     },
-  //     onStompError: (frame) => {
-  //       console.error(frame);
-  //     },
-  //     // reconnectDelay: 5000, // 자동 재 연결
-  //     heartbeatIncoming: 4000,
-  //     heartbeatOutgoing: 4000,
-  //   });
-
-  //   chatClient.current.activate();
-  // };
-  // const disconnect = () => {
-  //   chatClient.current.deactivate();
-  // };
-  // const subscribe = () => {
-  //   chatClient.current.subscribe(
-  //     `/topic/${chatRoomId}`,
-  //     ({ body }) => {
-  //       setChattingList((prev) => [...prev, JSON.parse(body)]);
-  //     },
-  //     { "auto-delete": true, durable: false, exclusive: false }
-  //   );
-  // };
-  // const publish = (chat) => {
-  //   if (!chatClient.current.connected) {
-  //     return;
-  //   }
-
-  //   chatClient.current.publish({
-  //     destination: `/pub/chat/message/${chatRoomId}`,
-  //     body: JSON.stringify({
-  //       content: chat,
-  //       memberId: jwtDecode(getCookie("accessToken")).memberId,
-  //     }),
-  //   });
-
-  //   setChat("");
-  // };
 
   useEffect(() => {
     connect();
 
     return () => disconnect();
   }, []);
+
   useEffect(() => {
     if (data) {
-      setChattingList(data.messages.map((chat) => chat.content));
+      setChatList(data.messages.map((chat) => chat.content));
     }
   }, [data]);
+
   if (error) {
     return <div>{error.message}</div>;
   }
   return (
     <div>
       <ul>
-        {chattingList.map((chat, index) => (
+        {chatList.map((chat, index) => (
           <li key={index}>{chat}</li>
         ))}
       </ul>
--- src/components/Chat/Chatting/useChat.jsx
+++ src/components/Chat/Chatting/useChat.jsx
@@ -1,21 +1,21 @@
 import { useEffect, useRef } from "react";
 import { Stomp } from "@stomp/stompjs";
 import SockJS from "sockjs-client";
-import { jwtDecode } from "jwt-decode";
-import { getCookie } from "../../../utils/cookie";
 
-export default function useChat(chatRoomId, setChattingList, setChat) {
+export default function useChat(
+  chatRoomId,
+  memberId,
+  setChattingList,
+  setChat
+) {
   const client = useRef();
 
   useEffect(() => {
-    const socket = new SockJS("http://localhost:8080/ws"); // WebSocket endpoint
-    const stompClient = Stomp.over(socket);
-
-    client.current = stompClient;
+    client.current = Stomp.over(() => new SockJS("http://localhost:8080/ws"));
 
     return () => {
-      if (stompClient.connected) {
-        stompClient.disconnect();
+      if (client.current.connected) {
+        client.current.disconnect();
       }
     };
   }, []);
@@ -23,18 +23,13 @@ export default function useChat(chatRoomId, setChattingList, setChat) {
   const connect = () => {
     client.current.connect(
       {
-        memberId: jwtDecode(getCookie("accessToken")).memberId,
+        memberId,
         chatRoomId,
       },
       () => {
-        console.log("Connected to WebSocket");
-        // Subscribe to the chat topic
         client.current.subscribe(
           `/topic/${chatRoomId}`,
           ({ body }) => {
-            // Handle incoming messages
-            // console.log('Received message:', JSON.parse(message.body));
-            // You can update your React state with the new message here
             setChattingList((prev) => [...prev, JSON.parse(body).content]);
           },
           { "auto-delete": true, durable: false, exclusive: false }
@@ -53,7 +48,7 @@ export default function useChat(chatRoomId, setChattingList, setChat) {
       {},
       JSON.stringify({
         content: message,
-        memberId: jwtDecode(getCookie("accessToken")).memberId,
+        memberId,
       })
     );
     setChat("");
