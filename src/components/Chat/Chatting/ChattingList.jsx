64e54e1962173df883575887d1943ea970794a1d
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2024-03-16 17:50:40 +0900

    Feat: 채팅 추가 데이터 받아올 때 스크롤 위치 고정
    - 채팅방 변경 시 캐시로 인해 정상동작하지 않아 chatRoomId가 변경될 때 캐시 삭제

--- src/components/Chat/Chatting/ChattingList.jsx
+++ src/components/Chat/Chatting/ChattingList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useLayoutEffect, useRef } from "react";
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { CHAT_QUERY_KEYS } from "../../../constants/queryKeys";
@@ -6,6 +6,7 @@ import useIntersectionObserver from "../../../hooks/useIntersectionObserver";
 import { getChattingWithCursor } from "../../../apis/chat";
 import { Spinner } from "@material-tailwind/react";
 import ChattingItem from "./ChattingItem";
+import { queryClient } from "../../../apis/queryClient";
 
 export default function ChattingList({
   chatRoomId,
@@ -17,6 +18,7 @@ export default function ChattingList({
   const observerRef = useRef(null);
   const chatListContainerRef = useRef(null);
   const chatListRef = useRef([]);
+  const [prevScrollHeight, setPrevScrollHeight] = useState(0);
   const { fetchNextPage, hasNextPage, isFetchingNextPage, data, error } =
     useInfiniteQuery({
       queryKey: CHAT_QUERY_KEYS.chatDataWithCursor(chatRoomId),
@@ -29,7 +31,21 @@ export default function ChattingList({
       getNextPageParam: (lastPage) =>
         lastPage.messages.length > 0 ? lastPage.messages[0].sendTime : null,
     });
-  const [observe, unobserve] = useIntersectionObserver(fetchNextPage);
+  const [observe, unobserve] = useIntersectionObserver(() => {
+    fetchNextPage();
+    const scrollHeight = chatListContainerRef.current?.scrollHeight;
+    setPrevScrollHeight(scrollHeight);
+  });
+
+  useLayoutEffect(() => {
+    if (!prevScrollHeight) return;
+
+    const scrollHeight = chatListContainerRef.current.scrollHeight;
+    chatListContainerRef.current.scrollTo({
+      top: scrollHeight - prevScrollHeight,
+    });
+    return () => setPrevScrollHeight(0);
+  }, [chatList, prevScrollHeight]);
 
   useLayoutEffect(() => {
     if (chatList.length === 0 || firstChatData.length !== chatList.length)
@@ -40,6 +56,7 @@ export default function ChattingList({
     });
   }, [chatList, unreadCount, firstChatData]);
 
+  // hasNextPage 넣어서 테스트
   useEffect(() => {
     const dataLength = data.pages.length;
     if (dataLength === 1) return;
@@ -54,6 +71,10 @@ export default function ChattingList({
 
     return () => unobserve(observer);
   }, [observerRef, hasNextPage, observe, unobserve]);
+
+  useEffect(() => {
+    queryClient.removeQueries(CHAT_QUERY_KEYS.chatDataWithCursor(chatRoomId));
+  }, [chatRoomId]);
 
   if (error) {
     return <div>{error.message}</div>;
