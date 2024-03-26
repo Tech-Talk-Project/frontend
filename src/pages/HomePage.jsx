9e14c48dc3a0bb92ec5491426f13d23e97fda99f
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2024-03-26 15:40:40 +0900

    Fix: 비로그인 상태에서 채팅방 생성 버튼 숨기기

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,11 +10,13 @@ import Button from "../components/Common/Button";
 import useModal from "../hooks/useModal";
 import CreateChatButtonGroup from "../components/Main/Common/CreateChatButtonGroup";
 import UserGridSkeleton from "../components/Main/User/Skeleton/UserGridSkeleton";
+import { isLoggedInState } from "../recoil/atoms/auth";
 
 export default function HomePage() {
   const [isOpen, handleModalClick] = useModal();
   const [filters, setFilters] = useState([]);
   const filter = useRecoilValue(filterState);
+  const isLoggedIn = useRecoilValue(isLoggedInState);
   const [createNewChat, setCreateNewChat] = useRecoilState(createNewChatState);
 
   const handleNewChatClick = () => {
@@ -55,7 +57,7 @@ export default function HomePage() {
           />
         )}
       </div>
-      {!createNewChat && (
+      {isLoggedIn && !createNewChat && (
         <Button
           variant="text"
           className="fixed bottom-8 right-7 md:left-7 md:right-auto p-3 text-white bg-brand hover:bg-white hover:text-brand rounded-full"
