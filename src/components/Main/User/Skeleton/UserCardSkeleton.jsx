015c8db17661c34a88bb544fc5caa6478b501f26
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2024-04-04 16:38:13 +0900

    Design: 메인 페이지 유저 리스트 스켈레톤 색상 어둡게 수정

--- src/components/Main/User/Skeleton/UserCardSkeleton.jsx
+++ src/components/Main/User/Skeleton/UserCardSkeleton.jsx
@@ -8,19 +8,19 @@ import {
 
 export default function UserCardSkeleton() {
   return (
-    <Card className="animate-pulse bg-dark_gray shadow-sm shadow-blue-gray-500">
+    <Card className="animate-pulse bg-dark_gray shadow-sm shadow-gray-900">
       <CardBody className="flex justify-between items-center">
         <Typography
           as="div"
           variant="h1"
-          className="h-6 w-28 rounded-md bg-blue-gray-800"
+          className="h-6 w-28 rounded-md bg-gray-900"
         >
           &nbsp;
         </Typography>
         <Typography
           as="div"
           variant="paragraph"
-          className="h-10 w-10 rounded-full bg-blue-gray-800"
+          className="h-10 w-10 rounded-full bg-gray-900"
         >
           &nbsp;
         </Typography>
@@ -29,7 +29,7 @@ export default function UserCardSkeleton() {
         <Typography
           as="div"
           variant="paragraph"
-          className="mb-2 h-10 w-full rounded-md bg-blue-gray-800"
+          className="mb-2 h-10 w-full rounded-md bg-gray-900"
         >
           &nbsp;
         </Typography>
