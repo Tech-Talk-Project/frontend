474a517ead4a4f508eac1d1bb8badbcd700194f6
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2024-04-19 16:01:42 +0900

    Style: MobileNavMenu 컴포넌트 함수 생성 후 전달

--- src/components/Common/Header/MobileNavMenu.jsx
+++ src/components/Common/Header/MobileNavMenu.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { ListItem } from "@material-tailwind/react";
 
-export default function MobileNavMenu({ menu, onMenuClick }) {
+export default function MobileNavMenu({ menu: { value, path }, onMenuClick }) {
+  const handleClick = () => onMenuClick(value, path);
+
   return (
-    <ListItem ripple={false} onClick={() => onMenuClick(menu.value, menu.path)}>
-      {menu.value}
+    <ListItem ripple={false} onClick={handleClick}>
+      {value}
     </ListItem>
   );
 }
