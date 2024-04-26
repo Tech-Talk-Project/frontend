36aa94d8822baf4eaf871957b8a88b8f2218044e
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2024-04-26 15:35:21 +0900

    Fix: URL을 통해 존재하지 않는 타입으로 접근 시 에러 처리
    - TypeProtectedRoute를 통해서 존재하지 않는 경우 "/board?type=project" 해당 주소로 이동

--- src/pages/ProtectedRoute/NoTypeProtectedRoute.jsx
+++ src/pages/ProtectedRoute/NoTypeProtectedRoute.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Navigate, useSearchParams } from "react-router-dom";
+import { BOARD_CATEGORIE_TYPES } from "../../constants/category";
 
 export default function NoTypeProtectedRoute({ children }) {
   const [searchParams] = useSearchParams();
+  const type = searchParams.get("type");
 
-  if (!searchParams.get("type")) {
+  if (!type || !BOARD_CATEGORIE_TYPES.includes(type)) {
     return <Navigate to="/board?type=project" replace />;
   }
   return children;
