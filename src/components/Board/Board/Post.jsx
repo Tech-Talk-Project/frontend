ffde991a6c58a2cbaf0a89378479482332005c8a
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2024-04-28 17:53:36 +0900

    Feat: 포스트 목록에서 포스트 선택 시 상세 보기 페이지로 이동

--- src/components/Board/Board/Post.jsx
+++ src/components/Board/Board/Post.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Chip, Typography } from "@material-tailwind/react";
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import Tags from "./Tags";
 import { BOARD_CATEGORIE_WITHOUT_TOGGLE_TYPES } from "../../../constants/category";
 import PostInfo from "./PostInfo";
 
 export default function Post({
   post: {
+    boardId,
     title,
     content,
     recruitmentActive,
@@ -19,11 +20,18 @@ export default function Post({
     commentCount,
   },
 }) {
+  const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const type = searchParams.get("type");
 
+  const handleClick = () => {
+    navigate(`/board/post/${boardId}?type=${type}`);
+  };
   return (
-    <li className="flex flex-col gap-1 p-3 border-b border-blue-gray-800">
+    <li
+      className="flex flex-col gap-1 p-3 border-b border-blue-gray-800 cursor-pointer"
+      onClick={handleClick}
+    >
       <div className="flex items-center gap-3">
         {!BOARD_CATEGORIE_WITHOUT_TOGGLE_TYPES.includes(type) && (
           <Chip
