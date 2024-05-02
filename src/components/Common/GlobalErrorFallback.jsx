62b7d5497651513b16e026e3b1ec578f4bf67924
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2024-05-02 16:55:32 +0900

    Fix: Global 에러 바운더리 구현
    - 400, 401: 접근 권한에 관한 에러
    - 500: 서버 에러

--- src/components/Common/GlobalErrorFallback.jsx
+++ src/components/Common/GlobalErrorFallback.jsx
@@ -3,34 +3,15 @@ import ErrorMessage from "../Error/ErrorMessage";
 import { getErrorMessage } from "../../utils/getErrorMessage";
 
 export default function GlobalErrorFallback({ error, reset }) {
-  const status = error.response?.status;
-  const { title, content, buttonMessage } = getErrorMessage(error);
-
-  if (status === 403) {
-    return (
-      <div className="flex justify-center items-center mx-auto max-w-7xl min-h-screen">
-        <main className="relative flex flex-col justify-center items-center gap-8 border px-20 py-12 border-line rounded-lg bg-opacity-50">
-          <ErrorMessage
-            title={title}
-            content={content}
-            buttonMessage={buttonMessage}
-            path={-1}
-          />
-        </main>
-      </div>
-    );
-  }
+  const { title, content, buttonMessage, path } = getErrorMessage(error);
 
   return (
-    <div className="flex justify-center items-center mx-auto max-w-7xl min-h-screen">
-      <main className="relative flex flex-col justify-center items-center gap-8 border px-20 py-12 border-line rounded-lg bg-opacity-50">
-        <ErrorMessage
-          title={title}
-          content={content}
-          buttonMessage={buttonMessage}
-          onRetry={reset}
-        />
-      </main>
-    </div>
+    <ErrorMessage
+      title={title}
+      content={content}
+      buttonMessage={buttonMessage}
+      path={path}
+      onRetry={reset}
+    />
   );
 }
