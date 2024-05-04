3bda4af09d3caff6749b91516c3b995cb1cc99d8
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2024-05-04 20:52:18 +0900

    Design: png 이미지 => svg 이미지로 변경

---
+++ src/assets/images/language/index.js
@@ -0,0 +1,262 @@
+import { ReactComponent as Android } from "./android.svg";
+import { ReactComponent as Angular } from "./angular.svg";
+import { ReactComponent as Ansible } from "./ansible.svg";
+import { ReactComponent as Bitbucket } from "./bitbucket.svg";
+import { ReactComponent as Csharp } from "./csharp.svg";
+import { ReactComponent as Cplusplus } from "./cplusplus.svg";
+import { ReactComponent as Css } from "./css.svg";
+import { ReactComponent as Django } from "./django.svg";
+import { ReactComponent as Docker } from "./docker.svg";
+import { ReactComponent as Express } from "./express.svg";
+import { ReactComponent as Firebase } from "./firebase.svg";
+import { ReactComponent as Flutter } from "./flutter.svg";
+import { ReactComponent as Git } from "./git.svg";
+import { ReactComponent as Github } from "./github.svg";
+import { ReactComponent as Gitlab } from "./gitlab.svg";
+import { ReactComponent as Godot } from "./godot.svg";
+import { ReactComponent as Html } from "./html.svg";
+import { ReactComponent as Java } from "./java.svg";
+import { ReactComponent as Javascript } from "./javascript.svg";
+import { ReactComponent as Jenkins } from "./jenkins.svg";
+import { ReactComponent as Keras } from "./keras.svg";
+import { ReactComponent as Kotlin } from "./kotlin.svg";
+import { ReactComponent as Kubernetes } from "./kubernetes.svg";
+import { ReactComponent as Laravel } from "./laravel.svg";
+import { ReactComponent as Less } from "./less.svg";
+import { ReactComponent as Mongo } from "./mongo.svg";
+import { ReactComponent as Mysql } from "./mysql.svg";
+import { ReactComponent as Nodejs } from "./nodejs.svg";
+import { ReactComponent as Opencv } from "./opencv.svg";
+import { ReactComponent as Oracle } from "./oracle.svg";
+import { ReactComponent as Owasp } from "./owasp.svg";
+import { ReactComponent as Php } from "./php.svg";
+import { ReactComponent as Postgresql } from "./postgresql.svg";
+import { ReactComponent as Puppet } from "./puppet.svg";
+import { ReactComponent as Python } from "./python.svg";
+import { ReactComponent as Pytorch } from "./pytorch.svg";
+import { ReactComponent as React } from "./react.svg";
+import { ReactComponent as Redis } from "./redis.svg";
+import { ReactComponent as Ruby } from "./ruby.svg";
+import { ReactComponent as Sass } from "./sass.svg";
+import { ReactComponent as Scikit } from "./scikit.svg";
+import { ReactComponent as Snort } from "./snort.svg";
+import { ReactComponent as Spring } from "./spring.svg";
+import { ReactComponent as Sqlite } from "./sqlite.svg";
+import { ReactComponent as Swift } from "./swift.svg";
+import { ReactComponent as Tensorflow } from "./tensorflow.svg";
+import { ReactComponent as Typescript } from "./typescript.svg";
+import { ReactComponent as Unity } from "./unity.svg";
+import { ReactComponent as Unreal } from "./unreal.svg";
+import { ReactComponent as Vue } from "./vue.svg";
+import { ReactComponent as Xamarin } from "./xamarin.svg";
+
+export const languageSVG = {
+  android: {
+    borderColor: "border-[#95CF00]",
+    image: <Android width="2rem" height="2rem" />,
+  },
+  angular: {
+    borderColor: "border-[#DD0031]",
+    image: <Angular width="2rem" height="2rem" />,
+  },
+  ansible: {
+    borderColor: "border-[#FFF]",
+    image: <Ansible width="2rem" height="2rem" />,
+  },
+  bitbucket: {
+    borderColor: "border-[#2684FF]",
+    image: <Bitbucket width="2rem" height="2rem" />,
+  },
+  "c++": {
+    borderColor: "border-[#5C8DBC]",
+    image: <Cplusplus width="2rem" height="2rem" />,
+  },
+  "c#": {
+    borderColor: "border-[#A179DC]",
+    image: <Csharp width="2rem" height="2rem" />,
+  },
+  css: {
+    borderColor: "border-[#1172B8]",
+    image: <Css width="2rem" height="2rem" />,
+  },
+  django: {
+    borderColor: "border-[#44b78b]",
+    image: <Django width="2rem" height="2rem" />,
+  },
+  docker: {
+    borderColor: "border-[#2396ED]",
+    image: <Docker width="2rem" height="2rem" />,
+  },
+  "express.js": {
+    borderColor: "border-[#FFF]",
+    image: <Express width="2rem" height="2rem" />,
+  },
+  firebase: {
+    borderColor: "border-[#FFC24A]",
+    image: <Firebase width="2rem" height="2rem" />,
+  },
+  flutter: {
+    borderColor: "border-[#40D0FD]",
+    image: <Flutter width="2rem" height="2rem" />,
+  },
+  git: {
+    borderColor: "border-[#EE513B]",
+    image: <Git width="2rem" height="2rem" />,
+  },
+  github: {
+    borderColor: "border-[#FFF]",
+    image: <Github width="2rem" height="2rem" />,
+  },
+  gitlab: {
+    borderColor: "border-[#FC6D26]",
+    image: <Gitlab width="2rem" height="2rem" />,
+  },
+  godot: {
+    borderColor: "border-[#478CBF]",
+    image: <Godot width="2rem" height="2rem" />,
+  },
+  html: {
+    borderColor: "border-[#E44D26]",
+    image: <Html width="2rem" height="2rem" />,
+  },
+  java: {
+    borderColor: "border-[#E76F00]",
+    image: <Java width="2rem" height="2rem" />,
+  },
+  javascript: {
+    borderColor: "border-[#F7DF1E]",
+    image: <Javascript width="2rem" height="2rem" />,
+  },
+  jenkins: {
+    borderColor: "border-[#F0D6B7]",
+    image: <Jenkins width="2rem" height="2rem" />,
+  },
+  keras: {
+    borderColor: "border-[#D00000]",
+    image: <Keras width="2rem" height="2rem" />,
+  },
+  kotlin: {
+    borderColor: "border-[#FFF]",
+    image: <Kotlin width="2rem" height="2rem" />,
+  },
+  kubernetes: {
+    borderColor: "border-[#326DE6]",
+    image: <Kubernetes width="2rem" height="2rem" />,
+  },
+  laravel: {
+    borderColor: "border-[#FFF]",
+    image: <Laravel width="2rem" height="2rem" />,
+  },
+  less: {
+    borderColor: "border-[#F6F6F6]",
+    image: <Less width="2rem" height="2rem" />,
+  },
+  mongodb: {
+    borderColor: "border-[#FFF]",
+    image: <Mongo width="2rem" height="2rem" />,
+  },
+  mysql: {
+    borderColor: "border-[#5D87A1]",
+    image: <Mysql width="2rem" height="2rem" />,
+  },
+  "node.js": {
+    borderColor: "border-[#8CC84B]",
+    image: <Nodejs width="2rem" height="2rem" />,
+  },
+  opencv: {
+    borderColor: "border-[#FF0101]",
+    image: <Opencv width="2rem" height="2rem" />,
+  },
+  oracle: {
+    borderColor: "border-[#EA1B22]",
+    image: <Oracle width="2rem" height="2rem" />,
+  },
+  "owasp zap": {
+    borderColor: "border-[#FFF]",
+    image: <Owasp width="2rem" height="2rem" />,
+  },
+  php: {
+    borderColor: "border-[#8993BE]",
+    image: <Php width="2rem" height="2rem" />,
+  },
+  postgresql: {
+    borderColor: "border-[#336791]",
+    image: <Postgresql width="2rem" height="2rem" />,
+  },
+  puppet: {
+    borderColor: "border-[#FFAE1A]",
+    image: <Puppet width="2rem" height="2rem" />,
+  },
+  python: {
+    borderColor: "border-[#327EBD]",
+    image: <Python width="2rem" height="2rem" />,
+  },
+  pytorch: {
+    borderColor: "border-[#EE4C2C]",
+    image: <Pytorch width="2rem" height="2rem" />,
+  },
+  react: {
+    borderColor: "border-[#53C1DE]",
+    image: <React width="2rem" height="2rem" />,
+  },
+  "react native": {
+    borderColor: "border-[#53C1DE]",
+    image: <React width="2rem" height="2rem" />,
+  },
+  redis: {
+    borderColor: "border-[#912626]",
+    image: <Redis width="2rem" height="2rem" />,
+  },
+  ruby: {
+    borderColor: "border-[#9E1209]",
+    image: <Ruby width="2rem" height="2rem" />,
+  },
+  sass: {
+    borderColor: "border-[#CD6799]",
+    image: <Sass width="2rem" height="2rem" />,
+  },
+  "scikit learn": {
+    borderColor: "border-[#F89939]",
+    image: <Scikit width="2rem" height="2rem" />,
+  },
+  snort: {
+    borderColor: "border-[#E6AAAA]",
+    image: <Snort width="2rem" height="2rem" />,
+  },
+  spring: {
+    borderColor: "border-[#70AD51]",
+    image: <Spring width="2rem" height="2rem" />,
+  },
+  sqlite: {
+    borderColor: "border-[#0F80CC]",
+    image: <Sqlite width="2rem" height="2rem" />,
+  },
+  swift: {
+    borderColor: "border-[#F88A36]",
+    image: <Swift width="2rem" height="2rem" />,
+  },
+  tensorflow: {
+    borderColor: "border-[#E55B2D]",
+    image: <Tensorflow width="2rem" height="2rem" />,
+  },
+  typescript: {
+    borderColor: "border-[#3178C6]",
+    image: <Typescript width="2rem" height="2rem" />,
+  },
+  unity: {
+    borderColor: "border-[#FFF]",
+    image: <Unity width="2rem" height="2rem" />,
+  },
+  "unreal engine": {
+    borderColor: "border-[#FFF]",
+    image: <Unreal width="2rem" height="2rem" />,
+  },
+  vue: {
+    borderColor: "border-[#41B883]",
+    image: <Vue width="2rem" height="2rem" />,
+  },
+  xamarin: {
+    borderColor: "border-[#3498DB]",
+    image: <Xamarin width="2rem" height="2rem" />,
+  },
+};
