b8881aed14be1e67c50bbd73ba9d2e099d985a2b
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2024-05-07 19:12:15 +0900

    FIx: 기술 스택 이미지 옵셔널 체이닝으로 안정성 확보

--- src/components/Common/Image/SkillImage.jsx
+++ src/components/Common/Image/SkillImage.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import { languageSVG } from "../../../assets/images/language";
 
 export default function SkillImage({ language }) {
-  const { borderColor, image } = languageSVG[language.toLowerCase()];
+  const languageIcon = languageSVG[language.toLowerCase()];
 
   return (
-    <article className={`rounded-full border ${borderColor} overflow-hidden`}>
-      {image}
+    <article
+      className={`rounded-full border ${
+        languageIcon?.borderColor || ""
+      } overflow-hidden`}
+    >
+      {languageIcon?.image}
     </article>
   );
 }
